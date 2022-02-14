f2c6fc436bccbca2dce7f3bef5a7a16fe20039f1
AGBT-7274 <99356526+AGBT-7274@users.noreply.github.com>
2022-02-14 11:22:28 -0800

    Create baath.rb

---
+++ baath.rb
@@ -0,0 +1,36 @@
+synth :piano
+use_bpm 150
+
+3.times do
+  play e4, amp: 5
+  play e4, amp: 5
+  sleep 1
+  play f5, amp: 5
+  play f5, amp: 5
+  sleep 1
+  play b4, amp: 5
+  play b4, amp: 5
+  sleep 1
+  play c5, amp: 5
+  play c5, amp: 5
+  sleep 1
+  play a4, amp: 5
+  sleep 1
+  play f4, amp: 5
+  sleep 1
+end
+
+
+play d5, amp: 5
+play d5, amp: 5
+sleep 1
+play a4, amp: 5
+play a4, amp: 5
+sleep 1
+play e5, amp: 5
+play e5, amp: 5
+sleep 1
+play a5, amp: 5
+sleep 1
+play g5, amp: 5
+sleep 1
