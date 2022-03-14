586f6a1f2252459b130a52f7ea3809b32a00d226
AGBT-7274 <99356526+AGBT-7274@users.noreply.github.com>
2022-03-14 09:09:20 -0700

    Update Stripes.rb

--- Stripes.rb
+++ Stripes.rb
@@ -1,16 +1,21 @@
-use_bpm 160
-use_synth :saw
-Notes = ["c2", "e2", "g2","b2", "c3", "b2", "g2", "e2"]
+#Challenge_g
+#Seven Nation Army by The White Stripes
+
+use_bpm 120
+use_synth :fm
+use_synth_defaults sustain: 0.25
+beep = ["e3", "e3", "g3", "e3", 0, "d3", "c3", "b2"]
+boop = [1.5,0.5, 0.75, 0.25, 0.5, 0.5, 2, 2]
 index = 0
+outdex = 0
 
-
-live_loop :main_theme do
+live_loop :white_stripes do
 8.times do
-with_fx :distortion do
-play (Notes[index])
-sleep 0.5
-index = index +1
-end
-end
-index = 0
+play (beep [index])
+sleep (boop [bowl])
+index = index + 1
+outdex = bowl + 1
+ end
+ index = 0
+ bowl = 0
 end
