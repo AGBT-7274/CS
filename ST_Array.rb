88236d1abbff30b0c3eda29c1c5660e4e140a95c
AGBT-7274 <99356526+AGBT-7274@users.noreply.github.com>
2022-03-14 08:51:30 -0700

    Update ST_Array.rb

--- ST_Array.rb
+++ ST_Array.rb
@@ -1,18 +1,17 @@
-# Stranger Things Main Theme
+#Challenge_G
+#Stranger Things Main Theme
 use_bpm 160
 use_synth :saw
-x = 0.5
+Notes = ["c2", "e2", "g2", "b2", "c3", "b2", "g2", "e2"]
 index = 0
 
-Array do :notes [c2,e2,g2,b2,c3,c3,c3,g2,e2]
-  end
-  live_loop :main_theme do
-    with_fx :distortion do
-      # YOUR TASK: Use arrays to shorten the 16 lines of code inside this live_loop to only 5 lines of code!
-      4.times do
-        play :notes[index]
-        sleep x
-        index = index + 1
-      end
-    end
-  end
+live_loop :main_theme do
+ with_fx :distortion do
+   7.times do
+     play (Notes [index])
+     sleep 0.5
+     index = index + 1
+   end
+ end
+ index = 0
+
