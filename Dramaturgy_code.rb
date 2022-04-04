1d64db178609d9f547fca4da143cf23125bd1d92
AGBT-7274 <99356526+AGBT-7274@users.noreply.github.com>
2022-04-04 10:35:28 -0700

    Create Dramaturgy_code.rb

---
+++ Dramaturgy_code.rb
@@ -0,0 +1,143 @@
+#Samples:
+
+
+#TEMPO & SYNTH
+use_bpm 140
+use_synth :piano
+x = -1
+y = 1
+z = 2
+
+#Panning notes
+#1
+play :f4
+play :d4
+sleep 0.75
+play :f4
+play :d4
+sleep 0.75
+play :f4
+play :d4
+sleep 0.5
+play :e4
+play :d4
+sleep 0.75
+play :e4
+play :d4
+sleep 0.75
+play :e4
+play :d4
+sleep 0.5
+play :a4
+play :e4
+sleep 0.75
+play :a4
+play :e4
+sleep 0.75
+
+#2
+play :a4
+play :e4
+sleep 0.75
+play :a4
+play :e4
+sleep 0.75
+play :a4
+play :e4
+sleep 0.5
+play :a4
+play :e4
+sleep 0.5
+
+
+#Guitar
+#1
+play :cb4
+play :gb3
+play :eb3
+sleep 1
+
+play :cb4
+sleep 0.25
+play :eb4
+sleep 0.25
+play :cb4
+sleep 0.25
+play :eb4
+sleep 0.25
+
+play :bb3
+play :f3
+play :db3
+sleep 1
+
+play :bb3
+sleep 0.25
+play :db3
+sleep 0.25
+play :bb3
+sleep 0.25
+play :db3
+sleep 0.25
+
+play :db4
+play :bb3
+play :gb3
+play :eb3
+sleep 1
+
+play :db4
+sleep 0.25
+play :eb3
+sleep 0.25
+play :eb4
+play :eb3
+sleep 0.25
+
+play :db4
+play :bb3
+play :gb3
+play :eb3
+sleep 0.5
+
+play :eb4
+play :eb3
+sleep 0.25
+play :eb4
+play :eb3
+sleep 0.25
+
+play :cb4
+sleep 0.25
+play :cb4
+sleep 0.25
+play :cb4
+sleep 0.25
+play :cb4
+sleep 0.25
+
+
+#2
+play :eb3
+play :eb2
+sleep 0.5
+play :eb3
+play :eb2
+sleep 0.5
+play :eb3
+play :eb2
+sleep 0.5
+play :eb3
+play :eb2
+sleep 0.5
+
+play :eb3
+play :eb2
+sleep 0.25
+
+
+#Vocals
+
+#Drum
+
+#Piano
