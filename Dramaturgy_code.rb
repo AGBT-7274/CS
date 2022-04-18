bd5cbe0d78d0b337c22acc9d6ea5e1fb3e9254d1
BT <99356526+AGBT-7274@users.noreply.github.com>
2022-04-18 06:56:37 -0700

    Update Dramaturgy_code.rb

--- Dramaturgy_code.rb
+++ Dramaturgy_code.rb
@@ -1,276 +1,276 @@
-#Samples:
-Drama_Instrumental = "C:/Users/ana_galarza/Downloads/Dramaturgy_instrumental (2).mp3"
-Drama_Drums ="C:/Users/ana_galarza/Downloads/Dramaturgy_Drums (2).mp3"
-Drama_Piano ="C:/Users/ana_galarza/Downloads/Dramaturgy_Piano (2).mp3"
-Drama_Vocal ="C:/Users/ana_galarza/Downloads/Dramaturgy_Vocals (2).mp3"
-Eve_Trans = "C:/Users/ana_galarza/Downloads/Outsider_E ve.mp3"
+# Welcome to Sonic Pi
+
+#Samples
+#Full Samples:
+drama_Instrumental = "/Users/jane/Desktop/CS_files/Dramaturgy_instrumental (2).mp3"
+drama_Drums = "/Users/jane/Desktop/CS_files/Dramaturgy_Drums (2).mp3"
+drama_Piano = "/Users/jane/Desktop/CS_files/Dramaturgy_Piano (2).mp3"
+drama_Vocal = "/Users/jane/Desktop/CS_files/Dramaturgy_Vocals (2).mp3"
+eve_Trans = "/Users/jane/Desktop/CS_files/Outsider_2nd.wav"
+
+#Specific Samples:
+luci = "/Users/jane/Desktop/CS_files/Drum1.wav"
+mammon = "/Users/jane/Desktop/CS_files/Drum2.mp3"
+levi = "/Users/jane/Desktop/CS_files/Drums3.wav"
+
+beel = "/Users/jane/Desktop/CS_files/Piano1.wav"
+belphie = "/Users/jane/Desktop/CS_files/Piano2.wav"
+
+lilith = "/Users/jane/Desktop/CS_files/Vocals.wav"
 
 #TEMPO & SYNTH
 use_bpm 140
 use_synth :piano
-x = -1
-y = 1
-z = 2
 
-triplet = ["a", "b", "c"]
-
-define :my_player do |a, b, c, d|
-  play a, amp:
-    sleep x
-  play b, amp:
-    sleep x
-  play c, amp:
-    sleep y
-  play d, amp:
-    sleep y
+define :pride do
+  sample luci
+  sleep 1
+end
+define :greed do
+  sample mammon
+  sleep 1
+end
+define :envy do
+  sample levi
+  sleep 1
 end
 
+eldest = ["pride", "greed", "envy"]
 
-live_loop : do
+define :simeon do |a|
+  play a
+  sleep 0.25
 end
-Define :Kaiju_No1 do
-  
+
+define :kaiju_No1 do
+  play :f4 ,pan: -1
+  play :d4 ,pan: -1
+  sleep 0.75
 end
-Define :Kaiju_No2 do
-  
+define :kaiju_No2 do
+  play :e4 ,pan: 1
+  play :d4 ,pan: 1
+  sleep 0.75
 end
-Define :Kaiju_No3 do
-  
+define :kaiju_No3 do
+  play :cb4
+  sleep 0.25
+  play :eb4
+  sleep 0.25
 end
-Define :Kaiju_No4 do
-  
+define :kaiju_No4 do
+  play :bb3
+  sleep 0.25
+  play :db3
+  sleep 0.25
 end
-Define :Kaiju_No5 do
-  
+define :kaiju_No5 do
+  play :db4
+  play :bb3
+  play :gb3
+  play :eb3
 end
+define :kaiju_No6 do
+  play :eb4
+  play :eb3
+  sleep 0.25
+end
+define :kaiju_No7 do
+  play :eb3
+  play :eb2
+end
+define :kaiju_No8 do
+  play :a4 ,pan: 1
+  play :e4 ,pan: 1
+end
+
 #Panning notes <Decremented Variable(Fade out) + Loop>
 #1
-play :f4 ,sustain : y, pan: x
-play :d4 ,sustain : y, pan: x
-sleep 0.75
-play :f4 ,sustain : y, pan: y
-play :d4 ,sustain : y, pan: y
-sleep 0.75
-play :f4 ,sustain : z, pan: x
-play :d4 ,sustain : z, pan: x
-sleep 0.5
-
-play :e4 ,sustain : y, pan: y
-play :d4 ,sustain : y, pan: y
-sleep 0.75
-play :e4 ,sustain : y, pan: x
-play :d4 ,sustain : y, pan: x
-sleep 0.75
-play :e4 ,sustain : z, pan: y
-play :d4 ,sustain : z, pan: y
-sleep 0.5
-
-play :a4 ,sustain : y, pan: x
-play :e4 ,sustain : y, pan: x
-sleep 0.75
-play :a4 ,sustain : y, pan: y
-play :e4 ,sustain : y, pan: y
-sleep 0.75
-
-#2
-play :a4 ,sustain : y, pan: x
-play :e4 ,sustain : y, pan: x
-sleep 0.75
-play :a4 ,sustain : y, pan: y
-play :e4 ,sustain : y, pan: y
-sleep 0.75
-play :a4 ,sustain : z, pan: x
-play :e4 ,sustain : z, pan: x
-sleep 0.5
-play :a4 ,sustain : z, pan: y
-play :e4 ,sustain : z, pan: y
-sleep 0.5
-
+2.times do
+  2.times do
+    kaiju_No1
+  end
+  play :f4 ,pan: -1
+  play :d4 ,pan: -1
+  sleep 0.5
+  
+  
+  2.times do
+    kaiju_No2
+  end
+  play :e4 ,pan: 1
+  play :d4 ,pan: 1
+  sleep 0.5
+  
+  play :a4 ,pan: -1
+  play :e4 ,pan: -1
+  sleep 0.75
+  play :a4 ,pan: 1
+  play :e4 ,pan: 1
+  sleep 0.75
+  
+  
+  #2
+  play :a4 ,pan: -1
+  play :e4 ,pan: -1
+  sleep 0.75
+  play :a4 ,pan: 1
+  play :e4 ,pan: 1
+  sleep 0.75
+  play :a4 ,pan: -1
+  play :e4 ,pan: -1
+  sleep 0.5
+  play :a4 ,pan: -1
+  play :e4 ,pan: -1
+  sleep 0.5
+end
 
 #Guitar
-#1_Runs <Function + Paremeterized Function>
-sleep 8
-play :cb4
-play :gb3
-play :eb3
-sleep 1
-
-play :cb4
-sleep 0.25
-play :eb4
-sleep 0.25
-play :cb4
-sleep 0.25
-play :eb4
-sleep 0.25
-
-play :bb3
-play :f3
-play :db3
-sleep 1
-
-play :bb3
-sleep 0.25
-play :db3
-sleep 0.25
-play :bb3
-sleep 0.25
-play :db3
-sleep 0.25
-
-play :db4
-play :bb3
-play :gb3
-play :eb3
-sleep 1
-
-play :db4
-sleep 0.25
-play :eb3
-sleep 0.25
-play :eb4
-play :eb3
-sleep 0.25
-
-play :db4
-play :bb3
-play :gb3
-play :eb3
-sleep 0.5
-
-play :eb4
-play :eb3
-sleep 0.25
-play :eb4
-play :eb3
-sleep 0.25
-
-play :cb4
-sleep 0.25
-play :cb4
-sleep 0.25
-play :cb4
-sleep 0.25
-play :cb4
-sleep 0.25
+#1_Runs
+live_loop :Guitar do
+  play :cb4
+  play :gb3
+  play :eb3
+  sleep 1
+  
+  2.times do
+    kaiju_No3
+  end
+  
+  play :bb3
+  play :f3
+  play :db3
+  sleep 1
+  
+  2.times do
+    kaiju_No4
+  end
+  
+  kaiju_No5
+  sleep 1
+  
+  play :db4
+  sleep 0.25
+  play :eb3
+  sleep 0.25
+  play :eb4
+  play :eb3
+  sleep 0.25
+  
+  kaiju_No5
+  sleep 0.5
+  
+  2.times do
+    kaiju_No6
+  end
+  
+  4.times do
+    play :cb4
+    sleep 0.25
+  end
+  stop
+end
 
 
-#2_Strings
-sleep 24
-play :eb3
-play :eb2
-sleep 0.5
-play :eb3
-play :eb2
-sleep 0.5
-play :eb3
-play :eb2
-sleep 0.5
-play :eb3
-play :eb2
-sleep 0.5
-
-play :eb3
-play :eb2
-sleep 0.25
-play :eb3
-play :eb2
-sleep 0.25
-play :eb3
-play :eb2
-sleep 0.25
-play :eb3
-play :eb2
-sleep 0.25
-play :eb3
-play :eb2
-sleep 1
-
-play :cb4
-play :gb3
-play :eb3
-sleep 1
-
-play :cb4
-sleep 0.25
-play :eb4
-sleep 0.25
-play :cb4
-sleep 0.25
-play :eb4
-sleep 0.25
-
-play :bb3
-play :f3
-play :db3
-sleep 1
-
-play :bb3
-sleep 0.25
-play :db3
-sleep 0.25
-play :bb3
-sleep 0.25
-play :db3
-sleep 0.25
-
-
-play :db4
-play :bb3
-play :gb3
-play :eb3
-sleep 1
-
-play :db4
-sleep 0.25
-play :eb3
-sleep 0.25
-play :eb4
-play :eb3
-sleep 0.5
-
-play :db4
-play :bb3
-play :gb3
-play :eb3
-sleep 0.5
-play :db4
-play :db3
-sleep 0.5
-
-
-play :eb4
-sleep 0.25
-play :eb3
-sleep 0.25
-play :eb4
-sleep 0.25
-play :eb3
-sleep 0.25
-
+#2_Strings <Function + Paremeterized Function>
+live_loop :Strings do
+  sleep 6
+  4.times do
+    kaiju_No7
+    sleep 0.5
+  end
+  
+  4.times do
+    kaiju_No7
+    sleep 0.25
+  end
+  
+  kaiju_No7
+  sleep 1
+  
+  play :cb4
+  play :gb3
+  play :eb3
+  sleep 1
+  
+  simeon :cb4
+  simeon :eb4
+  simeon :cb4
+  simeon :eb4
+  
+  play :bb3
+  play :f3
+  play :db3
+  sleep 1
+  
+  simeon :bb3
+  simeon :db3
+  simeon :bb3
+  simeon :db3
+  
+  play :db4
+  play :bb3
+  play :gb3
+  play :eb3
+  sleep 1
+  
+  simeon :db4
+  sleep 0.25
+  play :eb3
+  sleep 0.25
+  play :eb4
+  play :eb3
+  sleep 0.5
+  
+  play :db4
+  play :bb3
+  play :gb3
+  play :eb3
+  sleep 0.5
+  play :db4
+  play :db3
+  sleep 0.5
+  
+  
+  simeon :eb4
+  simeon :eb3
+  simeon :eb4
+  simeon :eb3
+  stop
+end
 
 #Vocals
-sleep 24
-sample Drama_Vocals
-sleep 1
+live_loop :vocals do
+  sleep 18
+  sample lilith
+  sleep 1
+  stop
+end
 
 #Drums <Loop + Array>
 #Drum_1
-sleep 14
-sample Drama_Drum1
-sleep 1
-
-#Drum_2
-sample Drama_Drum1
-sleep 1
-
-#Drum_3
-sample Drama_Drum1
-sleep 1
-
+live_loop :drums do
+  sleep 14
+  4.times do
+    index = 0
+    3.times do
+      sample(eldest[index])
+      sleep 1
+      index = index + 1
+    end
+  end
+  stop
+end
 #Piano <Decremented Variable(Fade in) + Loop>
-sleep 24
-4.times do
-  sample Drama_Piano
-  sleep 1
+live_loop :twins do
+  sleep 18
+  2.times do
+    x = 0.5
+    sample beel, amp: x
+    sleep 8
+    sample belphie, amp: x
+    sleep 8
+    x = x - 0.25
+  end
+  stop
 end
